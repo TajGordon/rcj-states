89b139095510831b49df133ebb7a5a2860810b97
Taj Gordon <tajgordon@gmail.com>
2025-09-13 00:49:53 +1000

    cursored some code to help with tofs, as well as near finished tof stuff i think 	modified:   config.py 	modified:   requirements.txt 	new file:   templates/tof_viewer.html 	modified:   tof_stuff.py 	new file:   tof_web_server.py

--- config.py
+++ config.py
@@ -15,7 +15,27 @@ if hostname == 'storm':
     tof_count = 8
     tof_addresses = [80, 88, 94, 91, 95, 93, 92, 86]
     tof_enabled = {80: True, 88: True, 94: True, 91: True, 95: False, 93: True, 92: True, 86: True} # my back one is covered so disable it
-    tof_angles = {80: 0, 88: math.radians(35), 94: math.radians(90), 91: math.radians(145), 95: math.radians(180), 93: math.radians(215), 92: math.radians(270), 86: math.radians(325)} #address -> radians, radians from north, starting at 0 and going clockwise
+    tof_angles = { # address -> radians, radians from north, starting at 0 and going clockwise
+        80: 0, 
+        88: math.radians(35), 
+        94: math.radians(90), 
+        91: math.radians(125), 
+        95: math.radians(180), 
+        93: math.radians(215), 
+        92: math.radians(270), 
+        86: math.radians(305)
+    } 
+    tof_offsets = { # address -> offset, in mm, like how far the tof is from the center of the robot
+        80: 42.5,
+        88: 111.5,
+        94: 57,
+        91: 111.5,
+        95: 42.5,
+        93: 111.5,
+        92: 57,
+        86: 111.5
+    }
+
 else: # assuming siddak bot rn
     motor_addresses = [27, 28, 30, 26]
     print(f'running on {hostname}')
--- tof_stuff.py
+++ tof_stuff.py
@@ -1,14 +1,77 @@
+from asyncio import wait_for
+from dataclasses import dataclass
 import time
 import struct
+from typing import Optional
 import board
 import busio
+import config
 
 READ_REG = 0x10 # address to write to to request data apparently
 
+@dataclass
+class ToFReading:
+    address: int
+    distance_mm: int
+    angle_rad: Optional[float]
+
+class ToFManager:
+    def __init__(self, i2c):
+        self.i2c = i2c
+        self.count = 8
+        self.addresses = config.tof_addresses
+        self.enabled = {addr: config.tof_enabled[addr] for addr in config.tof_addresses}
+        self.tofs = [SteelBarToF(i2c, addr, config.tof_angles[addr], config.tof_offsets[addr]) for addr in config.tof_addresses]
+        self.distances = {addr: 0 for addr in config.tof_addresses}
+        self.offsets = {addr: config.tof_offsets[addr] for addr in config.tof_addresses}
+        self.angles = {addr: config.tof_angles[addr] for addr in config.tof_addresses}
+    
+    def _read_one(self, addr, wait_for_new):
+        out_buf = bytes([READ_REG])
+        in_buf = bytearray(5)
+        while True:
+            self.i2c.writeto_then_readfrom(addr, out_buf, in_buf)
+            seq = in_buf[0]
+            distance_mm = struct.unpack('<i', in_buf[1:5])[0] 
+            if (not wait_for_new) or (self.last_sequences[addr] is None) or (seq != self.last_sequences[addr]):
+                self.last_sequences[addr] = seq
+                if distance_mm > 0:
+                    self.distances[addr] = distance_mm
+                    distance_mm += self.offsets[addr]
+                return self.distances[addr], seq
+    
+    def _batch_read(self, wait_for_new):
+        results = {}
+        for addr in self.addresses:
+            if not self.enabled.get(addr, True):
+                continue
+            try:
+                d, seq = self._read_one(addr, wait_for_new)
+                results[addr] = ToFReading(
+                    address=addr,
+                    distance_mm=d,
+                    angle_rad=self.angles.get(addr),
+                )
+            except Exception as e:
+                # who wants error handling anyways
+                pass
+        return results
+    
+    def batch_current(self):
+        return self._batch_read(wait_for_new=False)
+
+    def batch_next(self):
+        return self._batch_read(wait_for_new=True)
+
+    
+
 class SteelBarToF:
-    def __init__(self, i2c, address):
+    def __init__(self, i2c, address, angle, offset):
         self.i2c = i2c
         self.address = address
+        self.angle = angle
+        self.offset = offset
+        self.last_distance = 0
         self.last_sequence = None
     
     def _read(self, wait_for_new):
@@ -22,11 +85,15 @@ class SteelBarToF:
             distance_mm = struct.unpack('<i', read_buf[1:5])[0]
             if (not wait_for_new) or (self.last_sequence is None) or (seq != self.last_sequence):
                 self.last_sequence = seq
-                return distance_mm
+                if distance_mm > 0:
+                    last_distance = distance_mm
+                    return distance_mm
+                else:
+                    return last_distance
             
             time.sleep(0.00001)
     
-    def current_meeasurement(self):
+    def current_measurement(self):
         return self._read(wait_for_new=False)
     
     def next_measurement(self):
@@ -40,10 +107,6 @@ if __name__ == "__main__":
     while not i2c.try_lock():
         pass
     try:
-        sensor = SteelBarToF(i2c, 0x50)
-        while True:
-            d = sensor.next_measurement()
-            print(f"Distance: {d} mm")
-            time.sleep(0.1)
+        mgr = ToFManager(i2c)
     finally:
         i2c.unlock()
